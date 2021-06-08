54c995a4511c49411bcfce14db313a3ec4661b60
Gustavo Toporowicz <gh.toporowicz@gmail.com>
2021-06-07 20:37:33 -0500

    implement kafka producer

--- kafka-producer/src/main/kotlin/toporo/com/kafkaproducer/services/MessageService.kt
+++ kafka-producer/src/main/kotlin/toporo/com/kafkaproducer/services/MessageService.kt
@@ -1,12 +1,30 @@
 package toporo.com.kafkaproducer.services
 
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.stereotype.Service
 import toporo.com.kafkaproducer.entities.Message
+import java.util.*
 
 @Service
 class MessageService {
     open fun SendNewMessage(mesage : Message) : String {
 
+        var producer = createProducer()
+
+        var record = ProducerRecord(mesage.topic, "Message", mesage.message)
+        producer.send(record)
+
         return "Send message!"
+    }
+
+    fun createProducer(): Producer<String, String> {
+        val props = Properties()
+        props["bootstrap.servers"] = "localhost:9092"
+        props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        props["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+
+        return KafkaProducer(props)
     }
 }
