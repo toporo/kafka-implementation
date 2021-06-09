4d1098b90af07c1265f9b256632cf1a11c284b44
Gustavo Toporowicz <gh.toporowicz@gmail.com>
2021-06-08 22:44:34 -0300

    add init kafka-consumer

---
+++ kafka-consumer/src/main/kotlin/toporo/kafkaconsumer/KafkaConsumerApplication.kt
@@ -0,0 +1,11 @@
+package toporo.kafkaconsumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaConsumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaConsumerApplication>(*args)
+}
---
+++ kafka-consumer/src/test/kotlin/toporo/kafkaconsumer/KafkaConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package toporo.kafkaconsumer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
